2bd3e2ccffd555fb42fbd9f6e0e5c474fd8692ee
jgtaeeun <jas49258667@gmail.com>
2025-02-12 14:22:57 +0900

    8일차(2/12) 블록깨기 기본사항

--- README.md
+++ README.md
@@ -350,5 +350,7 @@ https://github.com/user-attachments/assets/5eaff7bf-9cf6-4e32-907e-bb21fc3c02b1
       
         https://github.com/user-attachments/assets/9e7fa95f-5a4e-4304-9f65-a07353c0880d
 
-        - 간단한 게임
+        - 간단한 게임(벽돌 깨기)
+
+        
         -  이미지, 사운드 등 리소스 활용 게임
--- day08/py05_blocks.py
+++ day08/py05_blocks.py
@@ -5,9 +5,10 @@ import sys
 import random
 import math
 
-
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 800
 pygame.init()
-screen_surface = pygame.display.set_mode(size=(600,400))
+screen_surface = pygame.display.set_mode(size=(SCREEN_WIDTH,SCREEN_HEIGHT ))
 pygame.display.set_icon(pygame.image.load('./day08/block.png'))
 FPSCLOCK= pygame.time.Clock()
 pygame.display.set_caption('Blocks Pygame')
@@ -29,13 +30,31 @@ def main():
 
     M_GAME_TITLE = bigFont.render('GAME START?' , True, 'white')
     M_GAME_SUBTITLE = smallFont.render('PRESS SPACE_BAR' , True, 'white')
-    
+    M_CLEAR = bigFont.render('CLEAR!' , True, 'green')
+    M_FAIL = bigFont.render('FAILED!' , True, 'red')
+
     while True:
-        screen_surface.fill(color= '#ffbaca')  
+        screen_surface.fill(color= 'black')  
         for event in pygame.event.get():   
             if event.type == QUIT :
                 pygame.quit()             
                 sys.exit()                  
+            elif event.type == KEYDOWN:
+                if event.key == K_LEFT:
+                    pass
+                elif event.key == K_RIGHT:
+                    pass
+                elif event.key == K_SPACE:
+                    is_game_str = True
+
+        if is_game_str ==False:
+            screen_surface.blit(M_GAME_TITLE, (SCREEN_WIDTH/2 - (400/2),SCREEN_HEIGHT/2 - (50/2)))
+            screen_surface.blit(M_GAME_SUBTITLE,(SCREEN_WIDTH/2 - (300/2),SCREEN_HEIGHT/2 +50))
+        
+        else :  # 게임시작 후 블록화면 구현, 볼이 움직이도록, 바도 움직이도록 
+            screen_surface.blit(M_CLEAR, (80,280))
+
+
         pygame.display.update()            
         FPSCLOCK.tick(30)                   
 
